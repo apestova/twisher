b8b7013984263e19fdc851e2725a69565dad5f21
apestova <1pest0vanna1@gmail.com>
2020-05-04 18:26:56 +0300

    add twisher/src/Posts.js

--- twisher/src/Posts.js
+++ twisher/src/Posts.js
@@ -5,6 +5,7 @@
             description: 'Более 76 тыс. человек во всем мире уже излечились от заболевания, спровоцированного новым коронавирусом, тогда как количество смертей превысило 6,4 тыс.',
             createdAt: new Date('2020-03-17T23:00:00'),
             author: 'Иван Иванов',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
             photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg'
         },
         {
@@ -12,18 +13,21 @@
             description: 'lol',
             createdAt: new Date('2020-03-17T23:01:00'),
             author: 'Петров Александр',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '3',
             description: 'Может быть однажды я приду на пары вовремя...',
             createdAt: new Date('2020-03-17T11:01:00'),
             author: 'Витилович Вихаил',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '4',
             description: 'Посмотрите на эту красоту!',
             createdAt: new Date('2020-03-17T13:01:00'),
             author: 'Радужная Долина',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
             photoLink: 'https://cs8.pikabu.ru/post_img/big/2018/04/20/10/1524246614168232071.jpg'
         },
         {
@@ -31,114 +35,132 @@
             description: 'видели ли вы вчера, какая красивая луна была?',
             createdAt: new Date('2020-03-17T15:01:00'),
             author: 'Александров Петя',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '6',
             description: 'доброе утро, я тока проснувся',
             createdAt: new Date('2020-03-17T12:01:00'),
             author: 'Бегун Юлий',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '7',
             description: 'это только седьмой пост, а у меня уже закончилась фантазия, что писать',
             createdAt: new Date('2020-04-14T14:15:00'),
             author: 'Пестова Анна',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '8',
             description: 'Только фанатичной толпой легко управлять!',
             createdAt: new Date('2020-04-14T14:15:00'),
             author: 'Апольф Ритлер',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '9',
             description: 'а впринципе это не так важно, так что поехали однотипные!!!!',
             createdAt: new Date('2020-04-14T14:10:00'),
             author: 'Пестова Анна',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '10',
             description: 'устала',
             createdAt: new Date('2020-04-14T14:21:00'),
             author: 'Пестова Анна',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '11',
             description: 'пара через 5 часов',
             createdAt: new Date('2020-04-14T14:21:30'),
             author: 'Пестова Анна',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '12',
             description: 'на дистанционном обучении заданий задают больше, чем раньше',
             createdAt: new Date('2020-04-14T14:22:00'),
             author: 'Пестова Анна',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '13',
             description: 'я даже не успеваю делать все вовремя',
             createdAt: new Date('2020-04-14T14:22:30'),
             author: 'Пестова Анна',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '14',
             description: 'и при этом некоторые преподаватели любят использовать фразы *ну вы дома ичего не делаете, так что вот вам еще задание*',
             createdAt: new Date('2020-04-14T14:23:00'),
             author: 'Пестова Анна',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '15',
             description: 'я просто снова хочу ходить в универ и все успевать(',
             createdAt: new Date('2020-04-14T14:24:00'),
             author: 'Пестова Анна',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '16',
             description: 'ладно, извините, меня стало слишком много в вашей ленте',
             createdAt: new Date('2020-04-14T14:25:00'),
             author: 'Пестова Анна',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '17',
             description: 'я ботан',
             createdAt: new Date('2020-04-14T14:26:00'),
             author: 'Бегун Юлий',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '18',
             description: 'юлий ботан',
             createdAt: new Date('2020-04-14T14:27:00'),
             author: 'Шикарный Макс',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '19',
             description: 'университетские чебуреки лучшие!!!',
             createdAt: new Date('2020-04-14T14:28:00'),
             author: 'Гном Ник',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
         {
             id: '20',
             description: 'miss you :c',
             createdAt: new Date('2020-04-14T14:28:30'),
             author: 'Гном Ник',
+            likes: ['Бегун Юлий', 'Иван Иванов'],
         },
 
-    ]
+    ];
     let empty={
         id: '',
         description: '',
         createdAt: new Date(),
         author: '',
-    }
+        likes: [],
+    };
     let newPost={
         id: '21',
         description: 'ff',
         createdAt: new Date('2020-03-14T14:28:30'),
         author: 'l k',
-    }
+        likes: [],
+    };
     let edPost={
         description: 'ff',
-    }
+    };
     function sortDate() {
         return posts.sort((a,b)=> a.createdAt.getTime() > b.createdAt.getTime() ? -1 : 1)
     }
@@ -169,7 +191,7 @@
             return false;
         if (post.description.length >= 200)
             return false;
-        if (post.author.length == 0)
+        if (post.author.length === 0)
             return false;
         return true;
     }
@@ -190,6 +212,8 @@
             return false;
         if (getPost(post.id))
             return false;
+        if (post.likes.length !== 0)
+            return false;
         posts[posts.length] = post;
         return true;
 
@@ -200,7 +224,7 @@
         if (!validatePost(getPost(id)))
             return false;
 
-        for (let key in getPost(id)) if ((key in post) && (key !== 'id') && (key !== 'author') && (key !== 'createdAt'))
+        for (let key in getPost(id)) if ((key in post) && (key !== 'id') && (key !== 'author') && (key !== 'createdAt') && (key !== 'likes'))
             getPost(id)[key] = post[key];
         return true;
 
@@ -223,7 +247,7 @@
     console.log(getPost('9'));
     console.log(validatePost(newPost));
    console.log(addPost(newPost));
-   console.log(editPost('2',edPost));
+   console.log(editPost('4',edPost));
    console.log( removePost('1'));
    console.log(posts);
 } ());
